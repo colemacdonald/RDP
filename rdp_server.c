d5c1f33516d4a04b627f9def52e59c16505e8b38
Cole Macdonald <colesinbox@gmail.com>
2017-03-09 11:17:42 -0800

    readded connect with errno

--- rdp_server.c
+++ rdp_server.c
@@ -84,12 +84,13 @@ int prepareSocket()
 	fromlen = sizeof(sa);
 	//end of copy
 
-	/*if(connect(sock, (struct sockaddr *) &sa, sizeof sa) != 0)
+	if(connect(sock, (struct sockaddr *) &sa, sizeof sa) != 0)
 	{
 		printf("Could not connect to receiver socket\n");
+		printf("%d\n", errno);
 		close(sock);
 		return FALSE;
-	}*/
+	}
 
 	/*if(bind(sock, (struct sockaddr *) &sa, sizeof sa) != 0)
 	{
