c82f69491f37062914ebecc2725ccf303089e8cc
Cole Macdonald <colesinbox@gmail.com>
2017-03-23 12:40:44 -0700

    small

--- rdp_server.c
+++ rdp_server.c
@@ -556,6 +556,7 @@ int main( int argc, char ** argv )
 					else*/
 						sendDataPacket(ackn + sent, MAX_PAYLOAD_SIZE, file_data);
 				}
+				state = states.LISTENING;
 			}
 			else
 				state = states.FINISH;
