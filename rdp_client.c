76e17dddf152c36282837010c99837e4e15b73df
Cole Macdonald <colesinbox@gmail.com>
2017-03-23 12:25:07 -0700

    small

--- rdp_client.c
+++ rdp_client.c
@@ -347,9 +347,9 @@ int main (int argc, char ** argv)
 		else if(state == states.LISTENING)
 		{
 			unsigned long long tmp = getTimeMS();
-			if(chunks_recv >= chunks_expecting || recv_timer + 2 * pkt_timeout > tmp)
+			if(chunks_recv >= chunks_expecting || recv_timer + 2 * pkt_timeout < tmp)
 			{
-				if(recv_timer + 2 * pkt_timeout > tmp)
+				if(recv_timer + 2 * pkt_timeout < tmp)
 					printf("timer: %llu --- now: %llu\n", recv_timer, tmp);
 				sendAckPacket(seq_expecting);
 			}
